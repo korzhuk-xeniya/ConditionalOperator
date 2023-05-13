b7b18d7f57f812b6534e3def504cdb6cc2ea3e49
Ксения <korzhuk.xeniya@yandex.ru>
2023-05-13 20:01:16 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,12 @@ public class Main {
     }
     public static void task1 () {
         System.out.println("Задача 1");
+        int age = 5;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
     }
     public static void task2 () {
         System.out.println("Задача 2");
